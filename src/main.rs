74044c7c2742e330162f0b3dcba7e53de87c948c
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-05-31 04:55:06 +0430

    Make Everything Async

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,33 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(name = "Proxy Checker", author = "Ariyan Eghbal <ariyan.eghbal@gmail.com>", version = "0.1.0", about = "Checks if proxyies work", long_about = None)]
 struct Args {
-    #[clap(short = 'v', long= "verbose")]
+    #[clap(short = 'v', long = "verbose")]
     verbose: bool,
+    #[clap(short = 't', long = "timeout", default_value = "5")]
+    timeout: u8,
 }
 
+
+async fn check_proxy(p: &String, timeout: u8) -> Result<(), reqwest::Error> {
+        let proxy = reqwest::Proxy::all(p)?; 
+        let client = reqwest::Client::builder()
+            .danger_accept_invalid_certs(true)
+            .proxy(proxy)
+            .build()?;
+        client.get("http://ifconfig.io/ip")
+            .header("Accept", "text/plain")
+            .header("User-Agent", "TEST")
+            .timeout(Duration::from_secs(timeout as u64))
+            .send()
+            .await?
+            .text()
+            .await?; 
+    Ok(())
+}
+
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     let args = Args::parse();
 
 
@@ -22,50 +43,27 @@ async fn main() -> Result<(), Box<dyn Error>> {
         proxies.push(line.expect("Could not read line from standard in"));
     }
 
-    for p in proxies{
-        if let Ok(proxy) = reqwest::Proxy::all(&p) { 
-            if let Ok(client) = reqwest::Client::builder().danger_accept_invalid_certs(true).proxy(proxy).build() { 
-                let resp = client.get("http://ifconfig.io/ip")
-                    .header("Accept", "text/plain")
-                    .header("User-Agent", "TEST")
-                    .timeout(Duration::from_secs(10))
-                    .send()
-                    .await;
-                if let Ok(resp) = resp {
-                    match resp.text().await {
-                        Ok(_) => println!("{}", p),
-                        Err(e) => {
-                            if args.verbose {
-                                eprintln!("{}: {}", p, e.to_string()) ;
-                            }
-                            continue;
+    let tasks = proxies.into_iter().map(|p| {
+        tokio::spawn(async move{
+            let res = check_proxy(&p, args.timeout).await;
+            match res {
+                Ok(_) => {
+                    println!("{}", p);
+                },
+                Err(e) => {
+                    if args.verbose {
+                        if e.is_timeout(){ 
+                            println!("{}: Timeout", p);
+                        }else{
+                            println!("{}: {}", p, e.source().unwrap().to_string());
                         }
                     }
-                }else if let Err(e) = resp {
-                    if e.is_timeout() {
-                        if args.verbose {
-                            eprintln!("{}: Timeout", p);
-                        }
-                    } else {
-                        if args.verbose {
-                            eprintln!("{}: {}", p, e.source().unwrap().to_string());
-                        }
-                        continue;
-                    }
                 }
-            } else {
-                if args.verbose {
-                    eprintln!("{} is not a valid proxy", p); 
-                }
-                continue;
             }
-        }else{
-            if args.verbose {
-                eprintln!("Cannot parse {}", p); 
-            }
-            continue
-        }
-        
+        })
+    }).collect::<Vec<_>>();
+
+    for task in tasks {
+        task.await.unwrap();
     }
-    Ok(())
 }
