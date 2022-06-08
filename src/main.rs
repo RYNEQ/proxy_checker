57347256a15e16bbd8009ab3348ab1e6e4fe4f76
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-06-08 07:08:50 +0430

    [feature]: repeated proxy check & minor fixes
    Now you can set number of repeats for each proxy (default: 5)

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,22 @@ struct Args {
     verbose: bool,
     #[clap(short = 't', long = "timeout", default_value = "5")]
     timeout: u8,
-    #[clap(short = 't', long = "target", default_value = "https://www.google.com")]
+    #[clap(short = 'T', long = "target", default_value = "https://www.google.com")]
     target_site: String,
-    #[clap(short = 'f', long = "file")]
+    #[clap(short = 'f', long = "file", help = "File containing proxies, one per line (without this proxies are read from stdin)")]
     proxy_file: Option<String>,
+    #[clap(short = 'r', long = "repeat", default_value = "5", help = "Number of times to repeat the test for each proxy")]
+    repeat: u8,
 }
 
 
-async fn check_proxy(p: &String, timeout: u8, target: String) -> Result<(), reqwest::Error> {
+async fn check_proxy(p: &String, timeout: u8, target: &String) -> Result<(), reqwest::Error> {
         let proxy = reqwest::Proxy::all(p)?; 
         let client = reqwest::Client::builder()
             .danger_accept_invalid_certs(true)
             .proxy(proxy)
             .build()?;
-        client.get(&target)
+        client.get(target)
             .header("Accept", "text/plain")
             .header("User-Agent", "TEST")
             .timeout(Duration::from_secs(timeout as u64))
@@ -35,6 +37,12 @@ async fn check_proxy(p: &String, timeout: u8, target: String) -> Result<(), reqw
     Ok(())
 }
 
+#[derive(Debug, PartialEq)]
+enum ProxyTestResult {
+    Success,
+    Timeout,
+    Failure(String),
+}
 
 #[tokio::main]
 async fn main() {
@@ -58,20 +66,32 @@ async fn main() {
     let tasks = proxies.into_iter().map(|p| {
         let target = args.target_site.clone();
         tokio::spawn(async move{
-            let res = check_proxy(&p, args.timeout, target).await;
-            match res {
-                Ok(_) => {
-                    println!("{}", p);
-                },
-                Err(e) => {
-                    if args.verbose {
+            let mut success_count = 0;
+            let mut result_list = vec![];
+            for _ in 0..args.repeat {
+                let res = check_proxy(&p, args.timeout, &target).await;
+                match res {
+                    Ok(_) => {
+                        success_count += 1;
+                        result_list.push(ProxyTestResult::Success);
+                    },
+                    Err(e) => {
                         if e.is_timeout(){ 
-                            println!("{}: Timeout", p);
+                            result_list.push(ProxyTestResult::Timeout);
                         }else{
-                            println!("{}: {}", p, e.source().unwrap().to_string());
+                            result_list.push(ProxyTestResult::Failure(e.source().unwrap().to_string()));
                         }
                     }
                 }
+            }
+            if args.verbose {
+                if success_count == args.repeat {
+                    println!("{}: Success", p);
+                }else{
+                    println!("{}: {}/{}", p, success_count, args.repeat);
+                }
+            }else{
+                println!("{}", p);
             }
         })
     }).collect::<Vec<_>>();
