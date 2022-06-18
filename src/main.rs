1f4e4f96a05a34d0dbb03e75064da36c1ec55ec7
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-06-18 05:15:13 +0430

    feat: multi-protocol test & content check
    Now it test for all http, https, socks4 and socks5
    It also is possible to give an string to check its existance in target page

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io::{self, BufRead, Read};
-use std::{error::Error,fs};
+use std::{error::Error,slice::Iter,fs};
 use std::time::Duration;
 use clap::Parser;
 
 
 #[derive(Parser, Debug)]
-#[clap(name = "Proxy Checker", author = "Ariyan Eghbal <ariyan.eghbal@gmail.com>", version = "0.2.0", about = "Checks if proxyies work", long_about = None)]
+#[clap(name = "Proxy Checker", author = "Ariyan Eghbal <ariyan.eghbal@gmail.com>", version = "0.3.0", about = "Checks if proxyies work", long_about = None)]
 struct Args {
     #[clap(short = 'v', long = "verbose")]
     verbose: bool,
@@ -13,35 +13,80 @@ struct Args {
     timeout: u8,
     #[clap(short = 'T', long = "target", default_value = "https://www.google.com")]
     target_site: String,
+    #[clap(short = 's', long = "string", help="String to search for in the target site")]
+    check_str: Option<String>,
     #[clap(short = 'f', long = "file", help = "File containing proxies, one per line (without this proxies are read from stdin)")]
     proxy_file: Option<String>,
     #[clap(short = 'r', long = "repeat", default_value = "5", help = "Number of times to repeat the test for each proxy")]
     repeat: u8,
 }
 
-
-async fn check_proxy(p: &String, timeout: u8, target: &String) -> Result<(), reqwest::Error> {
+/// Takes a proxy string and returns makes error if it fails to connect to target
+/// Returns Result<(), reqwest::Error>
+async fn check_proxy(p: &String, timeout: u8, target: &String, test_string: &Option<String>) -> Result<bool, reqwest::Error> {
         let proxy = reqwest::Proxy::all(p)?; 
         let client = reqwest::Client::builder()
             .danger_accept_invalid_certs(true)
             .proxy(proxy)
             .build()?;
-        client.get(target)
-            .header("Accept", "text/plain")
-            .header("User-Agent", "TEST")
-            .timeout(Duration::from_secs(timeout as u64))
-            .send()
-            .await?
-            .text()
-            .await?; 
-    Ok(())
+        let res = client.get(target)
+                 .header("Accept", "text/plain")
+                 .header("User-Agent", "TEST")
+                 .timeout(Duration::from_secs(timeout as u64))
+                 .send()
+                 .await?
+                 .text()
+                 .await?;
+        match test_string {
+            Some(s) => {
+                if res.contains(s) {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            },
+            None => Ok(true),
+        }
 }
 
 #[derive(Debug, PartialEq)]
 enum ProxyTestResult {
     Success,
     Timeout,
+    TextNotFound,
     Failure(String),
+}
+
+#[derive(Debug)]
+enum Scheme {
+    Http,
+    Https,
+    Socks4,
+    Socks5,
+}
+
+
+impl Scheme {
+    fn iter() -> Iter<'static, Scheme> {
+        static SCHEMES: [Scheme; 4] = [Scheme::Http, 
+                                       Scheme::Https, 
+                                       Scheme::Socks4, 
+                                       Scheme::Socks5];
+        SCHEMES.iter()
+    }
+
+    fn value(&self) -> &str {
+        match *self {
+            Scheme::Http => "http",
+            Scheme::Https => "https",
+            Scheme::Socks4 => "socks4",
+            Scheme::Socks5 => "socks5"
+        }
+    }
+}
+fn get_url_without_scheme(url: &String) -> String {
+    let url_without_scheme = url.split("://").last().unwrap();
+    url_without_scheme.to_string()
 }
 
 #[tokio::main]
@@ -63,35 +108,44 @@ async fn main() {
         }
     }
 
+
     let tasks = proxies.into_iter().map(|p| {
         let target = args.target_site.clone();
+        let check_str = args.check_str.clone();
         tokio::spawn(async move{
-            let mut success_count = 0;
             let mut result_list = vec![];
-            for _ in 0..args.repeat {
-                let res = check_proxy(&p, args.timeout, &target).await;
-                match res {
-                    Ok(_) => {
-                        success_count += 1;
-                        result_list.push(ProxyTestResult::Success);
-                    },
-                    Err(e) => {
-                        if e.is_timeout(){ 
-                            result_list.push(ProxyTestResult::Timeout);
-                        }else{
-                            result_list.push(ProxyTestResult::Failure(e.source().unwrap().to_string()));
+            for scheme in Scheme::iter() {
+                let mut success_count = 0;
+                let p_with_scheme = format!("{}://{}", scheme.value(), get_url_without_scheme(&p));
+                for _ in 0..args.repeat {
+                    let res = check_proxy(&p_with_scheme, args.timeout, &target, &check_str).await;
+                    match res {
+                        Ok(res) => {
+                            if res {
+                                success_count += 1;
+                                result_list.push((ProxyTestResult::Success, scheme));
+                            }else{
+                                result_list.push((ProxyTestResult::TextNotFound, scheme));
+                            }
+                        },
+                        Err(e) => {
+                            if e.is_timeout(){ 
+                                result_list.push((ProxyTestResult::Timeout, scheme));
+                            }else{
+                                result_list.push((ProxyTestResult::Failure(e.source().unwrap().to_string()), scheme));
+                            }
                         }
                     }
                 }
-            }
-            if args.verbose {
-                if success_count == args.repeat {
-                    println!("{}: Success", p);
-                }else{
-                    println!("{}: {}/{}", p, success_count, args.repeat);
+                if args.verbose {
+                    if success_count == args.repeat {
+                        println!("{}: Success", p_with_scheme);
+                    }else{
+                        println!("{}: {}/{}", p_with_scheme, success_count, args.repeat);
+                    }
+                }else if success_count > 0 {
+                    println!("{}", p_with_scheme);
                 }
-            }else{
-                println!("{}", p);
             }
         })
     }).collect::<Vec<_>>();
