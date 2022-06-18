67cd05e579a0148bbf0c417a48f0ae126d322a3f
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-06-18 05:19:32 +0430

    Version Up

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 
 
 #[derive(Parser, Debug)]
-#[clap(name = "Proxy Checker", author = "Ariyan Eghbal <ariyan.eghbal@gmail.com>", version = "0.3.0", about = "Checks if proxyies work", long_about = None)]
+#[clap(name = "Proxy Checker", author = "Ariyan Eghbal <ariyan.eghbal@gmail.com>", version = "0.4.0", about = "Checks if proxyies work", long_about = None)]
 struct Args {
     #[clap(short = 'v', long = "verbose")]
     verbose: bool,
